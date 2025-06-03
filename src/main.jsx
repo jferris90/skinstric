be8d49fb1ee1131b4e27090b4e612fd7ccc70a57
Jarrett Ferris <ferriswrites@gmail.com>
2025-06-03 15:25:46 -0700

    removed Strict Mode

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import 'aos/dist/aos.css';
 AOS.init();
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
 )
